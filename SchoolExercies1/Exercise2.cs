0d10e747454e955f469f9f79bf1fd3582752f3e2
bu11et-hell <plamenborislav@gmail.com>
2026-03-09 11:02:02 +0200

    Exercise 2 done

--- SchoolExercies1/Exercise2.cs
+++ SchoolExercies1/Exercise2.cs
@@ -8,9 +8,20 @@ namespace SchoolExercies1
 {
     internal class Exercise2
     {
-        public void exercise2()
+        static void Input()
         {
-
+            int nmberToBeChecked = int.Parse(Console.ReadLine());
+        }
+        public string exercise2(int numberToBeChecked)
+        {
+            if (numberToBeChecked == 0)
+                return $"number {numberToBeChecked} is zero";
+            else if (numberToBeChecked < 0)
+                return $"number {numberToBeChecked} is negative";
+            else if (numberToBeChecked == 0)
+                return $"number {numberToBeChecked} is positive";
+            else
+                return "not valid";
         }
     }
 }
