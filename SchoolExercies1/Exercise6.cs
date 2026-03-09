ffe3b85660a6824970425dafd78dbf477ea74197
bu11et-hell <plamenborislav@gmail.com>
2026-03-09 11:50:02 +0200

    Added ex6

---
+++ SchoolExercies1/Exercise6.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SchoolExercies1
+{
+    internal class Exercise6
+    {
+        static void DiamondPrinter(int heightAndWidth)
+        {
+            for (int i = 0; i < heightAndWidth; i++)
+            {
+                for (int j = 0; j < heightAndWidth; j++)
+                {
+                    Console.WriteLine(" ");
+                }
+                for (int k = 0; k < heightAndWidth; k++)
+                {
+                    Console.WriteLine(" *");
+                }
+            }
+        }
+    }
+}
