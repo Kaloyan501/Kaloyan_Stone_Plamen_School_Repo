94188737f714d7e8d6049635ab3326dc3a8dd08e
Stone63777 <stone63777@gmail.com>
2026-03-09 11:39:29 +0200

    Resolve merge conflict. Keeping local version

--- SchoolExercies1/Exercise_1_1.cs
+++ SchoolExercies1/Exercise_1_1.cs
@@ -8,9 +8,23 @@ namespace SchoolExercies1
 {
     internal class Exercise_1_1
     {
+        public void layerOne() 
+        {
+            Console.WriteLine("cash receipt \n----------------");
+        }
+        public void layerSecond()
+        {
+            Console.WriteLine("cherged to__________ \nrecieved by_______________");
+        }
+        public void layerThird()
+        {
+            Console.WriteLine("---------------- \ncash receipt ");
+        }
         public void exercise1_1()
         {
-
+            Console.WriteLine(layerOne);
+            Console.WriteLine(layerSecond);
+            Console.WriteLine(layerThird);
         }
     }
 }
