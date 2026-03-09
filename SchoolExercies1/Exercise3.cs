ab6613ac3306ef7b8713707635a2a5c9a3c3c961
Kaloyan Donev <kenef@invalid.com>
2026-03-09 10:44:57 +0200

    Upload project correctly

---
+++ SchoolExercies1/Exercise3.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SchoolExercies1
+{
+    internal class Exercise3
+    {
+        public static void exercise3() { 
+            
+        }
+    }
+}
--- SchoolExercies1/obj/Debug/net9.0/SchoolExercies1.AssemblyInfo.cs
+++ SchoolExercies1/obj/Debug/net9.0/SchoolExercies1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SchoolExercies1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+871c88b702b2296c2931a5756f071797951c4f8e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+034b7047bbba156c0dcfb651221e61cca9608781")]
 [assembly: System.Reflection.AssemblyProductAttribute("SchoolExercies1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SchoolExercies1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
