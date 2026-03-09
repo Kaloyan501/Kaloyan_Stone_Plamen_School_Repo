a151b48abe5e2d69f4493cc41cf37c351d246036
bu11et-hell <plamenborislav@gmail.com>
2026-03-09 11:38:03 +0200

    exercise 2 fixed

--- SchoolExercies1/Exercise2.cs
+++ SchoolExercies1/Exercise2.cs
@@ -8,20 +8,10 @@ namespace SchoolExercies1
 {
     internal class Exercise2
     {
-<<<<<<< HEAD
-        static int Input()
-        {
-            return int.Parse(Console.ReadLine());
-        }
-        public static string exercise2()
-        {
-            int numberToBeChecked = Input();
-=======
         public string exercise2()
         {
-            int nmberToBeChecked = int.Parse(Console.ReadLine());
+            int numberToBeChecked = int.Parse(Console.ReadLine());
 
->>>>>>> ed679d81c7b361d8e421678c28d776019b3a9ac1
             if (numberToBeChecked == 0)
                 return $"number {numberToBeChecked} is zero";
             else if (numberToBeChecked < 0)
