3e4b1f45d613cd9c5101145d9db6d25c1e7621d7
Kaloyan Donev <kenef@invalid.com>
2026-03-09 10:34:22 +0200

    Project base

---
+++ SchoolExercies1/SchoolExercies1/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace SchoolExercies1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
